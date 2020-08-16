00caede7b643f5bace394eb447951fc04307eafb
Steve Barnett <mooped@gmail.com>
2020-08-16 22:16:40 +0100

    Throw together a Kith character sheet (for Purple Planet adventures) as an example of adding character sheets to the DCC System for Foundry VTT.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kith sheets for Dungeon Crawl Classics in Foundry VTT
+
+Expands the [Dungeon Crawl Classics system](https://github.com/cyface/foundryvtt-dcc/) for the [Foundry Virtual Tabletop](https://foundryvtt.com) to include Kith character sheets.
+
+##### Maintainers
+* Steve Barnett (@mooped)
+
---
+++ module/dcc-kith.js
@@ -0,0 +1,14 @@
+/* global Actors */
+
+import * as KithSheets from './actor-sheets-kith.js'
+
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+Hooks.once('init', async function () {
+  console.log(`DCC | Initializing Dungeon Crawl Classics System`)
+
+  // Register sheet application classes
+  Actors.registerSheet('dcc-kith', KithSheets.ActorSheetKith)
+})
+
